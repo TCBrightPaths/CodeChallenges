40d4a71c6a1ad233c66692a689d48deca5e86fe8
Tamara Chance <tamara.n.chance@gmail.com>
2021-10-18 10:04:35 -0500

    completed but buggy

--- add2Zero.js
+++ add2Zero.js
@@ -1,12 +1,13 @@
 let arr1 = [1, 4, 11, 2, 37, (-4)];
 let arr2 = [0, 21, 33, 6, 0, (-9)];
 let arr3 = [0, 1, 2, 3, 4, 5];
-for (i = 0; i < arr1.length; i++) {
-    for (j = i+1; j < arr1.length; i++) {
-        if ( arr1[i] + arr1[j] === 0) {
+for (i = 0; i < arr3.length; i++) {
+    for (j = i+1; j < arr3.length - 1; i++) {
+        if ( arr3[i] + arr3[j] === 0) {
             console.log("true");
-        } else {
-            console.log("false");
         }
     }
+}
+if(arr1[i] + arr1[2]!== 0) {
+    console.log("false");
 }
