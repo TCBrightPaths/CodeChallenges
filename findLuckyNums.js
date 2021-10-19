4df724447e6f60613ea448bb644cedf4464acdd6
Tamara Chance <tamara.n.chance@gmail.com>
2021-10-19 09:59:58 -0500

    added function

--- findLuckyNums.js
+++ findLuckyNums.js
@@ -1,13 +1,8 @@
-function findLuckyNums(n) {
-    let newArray = [];
-  return Math.floor(Math.random() * n) + 1;
-}
-
+let newArray = [];
 function findLuckyNumbers(n) {
-    const options = [1,2,3,4,5,6,7,8,9,10];
-    let newArr = [];
-    for (i = 0; i < options.length; i++) {
-        
-        return Math.floor(Math.random() * n);
-    }
+    let result = Math.floor(Math.random() * 10 + 1);
+    newArray.push(result);
+    
 }
+findLuckyNumbers(8);
+console.log(newArray);
