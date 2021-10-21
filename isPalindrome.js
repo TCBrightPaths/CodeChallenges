635dbd0742db0c94de47dbf2b9de4e0fa0644019
Tamara Chance <tamara.n.chance@gmail.com>
2021-10-21 09:59:18 -0500

    init palindrome

---
+++ isPalindrome.js
@@ -0,0 +1,13 @@
+let arr = ["r","a","c","e","c","a","r"]
+let newArr = []
+function isPalindrome(arr) {
+    let j = arr.length -1
+    for(let i = 0; i < (arr.length -1)/2; i++) {
+        if(arr[i] !== arr[j]) {
+            return false
+        } 
+        j--;
+        return true;
+    }
+    
+isPalindrome("racecar");
