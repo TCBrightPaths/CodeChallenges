e50a05379735d43403e63e489602d8e8db5aeb5b
Tamara Chance <tamara.n.chance@gmail.com>
2021-10-25 09:43:44 -0500

    hasMoreVowels completed with ignore case

--- hasMoreVowels.js
+++ hasMoreVowels.js
@@ -3,8 +3,8 @@
 const hasMoreVowels = (word) => {
     //let vowels = ["a", "e", "i", "o", "u"];
     //let alpha  = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
-    let hasVowels = word.match(/['aeiou']/g);
-    let hasAlpha = word.match(/['bcdfghjklmnpqrstvwxyz']/g);
+    let hasVowels = word.match(/['aeiou']/ig);
+    let hasAlpha = word.match(/['bcdfghjklmnpqrstvwxyz']/ig);
     if(hasVowels.length > hasAlpha.length) {
         return true
     } else {
@@ -12,5 +12,5 @@ const hasMoreVowels = (word) => {
     }
 }
 
-console.log(hasMoreVowels('yay'));
+console.log(hasMoreVowels('Aal'));
 
