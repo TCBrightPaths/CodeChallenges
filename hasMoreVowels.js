75a5cc0a036274a9af362bbc97410bc7ae391f34
Tamara Chance <tamara.n.chance@gmail.com>
2021-10-25 09:39:46 -0500

    hasMoreVowels completed

--- hasMoreVowels.js
+++ hasMoreVowels.js
@@ -1,1 +1,16 @@
 //hasMoreVowels
+
+const hasMoreVowels = (word) => {
+    //let vowels = ["a", "e", "i", "o", "u"];
+    //let alpha  = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
+    let hasVowels = word.match(/['aeiou']/g);
+    let hasAlpha = word.match(/['bcdfghjklmnpqrstvwxyz']/g);
+    if(hasVowels.length > hasAlpha.length) {
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(hasMoreVowels('yay'));
+
