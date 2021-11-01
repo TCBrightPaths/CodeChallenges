f2c80f08f0199d096132545ccb06b1cb305f3a60
Tamara Chance <tamara.n.chance@gmail.com>
2021-11-01 09:19:40 -0500

    init

---
+++ decodeString.js
@@ -0,0 +1,16 @@
+const decoder = code => {
+    let splitStr = code.split('')
+    let result = ''
+  
+    for (let i = 0; i < splitStr.length; i++) {
+      if (!isNaN(+splitStr[i])) {
+        i += +splitStr[i]
+      } else {
+        result += splitStr[i]
+      }
+    }
+  
+    return result
+  }
+
+  console.log(decoder("2fcjjm"));
