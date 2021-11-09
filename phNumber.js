c8710c8ccec78b2b9ef190a3ca5f2034aaef2fd7
Tamara Chance <tamara.n.chance@gmail.com>
2021-11-09 09:40:02 -0600

    created phone number

--- phNumber.js
+++ phNumber.js
@@ -1,13 +1,13 @@
 //Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
 
-function createPhoneNumber (array)
+function createPhoneNumber (array) {
     
-    array.toString()
-    // '(' at position 0
-    //add ') ' after position 2
-    //add '-' after position 5  
-    //return a string out of the array
-
+    array.unshift('(')
+    array.splice(4, 0, ') ') //add ') ' after position 2
+    array.splice(8, 0, '-')  //add '-' after position 5  
+    let ph = array.join(" ") //return a string out of the array
+    console.log(ph)
+}
 
 createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) 
 
