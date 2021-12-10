5e21b96ac50ac82c1375c5ac0afe26ef32223098
Tamara Chance <tamara.n.chance@gmail.com>
2021-12-10 14:57:41 -0600

    added huge input file but my answer is incorrect

--- sonarSweep.js
+++ sonarSweep.js
@@ -1,3 +1,22 @@
+let fs = require("fs");
+
+let nums = () => {
+    try {
+        let data = fs.readFileSync("./sonarSweep.txt", "utf8")
+        data = data.split("")
+        let increasedVals = []
+        for (i = 0; i < data.length - 1; i++) {
+            if(data[i] < data[i+1]) {
+                increasedVals.push(data[i+1])
+            }
+        }
+        console.log(increasedVals.length)
+    } catch(err) {
+        console.log('ERROR:', err)
+    }
+}
+
+nums();
 let arr1 = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
 
 //Write a function that takes in an (array)
@@ -17,4 +36,4 @@ const sonarSweep = (arr) => {
     console.log(increasedVals.length)
 }
 
-sonarSweep(arr1)
+//sonarSweep(arr1)
