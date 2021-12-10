058016bafa666ca6454b6fa2318b003d01eaae7b
Tamara Chance <tamara.n.chance@gmail.com>
2021-12-10 14:43:45 -0600

    init

---
+++ dive.js
@@ -0,0 +1,27 @@
+//horizontal position starts at 0
+//depth starts at 0
+//let x and y variables control increment
+//forward increments horizontal position
+//up decrements depth
+//down increments depth
+//product equals position times depth
+//write a function that takes in (a direction, a value)
+    //if direction equals forward
+        //add to horizonal position by value
+        //return hposition
+    //if direction equals up
+        //decrement depth by value
+        //return depth
+    //if direction equals down
+        //increment depth by value
+        //return depth
+    //if any other direction
+        //log that is not a valid move
+    //let depth equal the new value
+//write another function that takes in an (object array)
+    //calls above function on each value
+
+const dive = (dir, num) => {
+    switch()
+} 
+    
